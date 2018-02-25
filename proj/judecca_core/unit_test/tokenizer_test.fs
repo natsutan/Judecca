3215caf1bc453df32fdae87585a889d4dcdfa62d
natsutan <YRW03704@nifty.com>
2018-02-25 09:56:43 +0900

    連続テストが実行できるようになった

--- proj/judecca_core/unit_test/tokenizer_test.fs
+++ proj/judecca_core/unit_test/tokenizer_test.fs
@@ -3,11 +3,22 @@
 open NUnit.Framework
 open tokenizer
 
-[<Test>]
-let tokenizer_test0() = 
-    let s = "1 2 3　4"
-    let result = tokenizer.tokenize s
-    Assert.AreEqual(result, ["1";"2";"3";"4"])
+type token_test_data(indata:string, expected:string list) =
+    let _indata = indata
+    let _expected = expected
+    member this.indata = _indata
+    member this.expected = _expected
+
+
+let test1 = token_test_data("A 2 3 4" , ["A";"2";"3";"4"])
+let test2 = token_test_data("1 2" , ["1";"2"])
+
+let token_test_sorces = [test1; test2]
+
+[<TestCaseSource("token_test_sorces")>]
+let tokenizer_test0(pair:token_test_data) = 
+    let result = tokenizer.tokenize pair.indata
+    Assert.AreEqual(result,  pair.expected)
 
 [<EntryPoint>]
 let main argv = 
