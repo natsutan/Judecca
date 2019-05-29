c4fefb824f346be0d69f14c873c34fb16c4cce70
natsutan <YRW03704@nifty.com>
2019-05-29 16:49:58 +0900

    IrNodeを追加できるように変更

--- judecca/src/liveness/liveness_analysis.rs
+++ judecca/src/liveness/liveness_analysis.rs
@@ -1,24 +1,55 @@
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
 use std::collections::HashSet;
 
 use crate::onnx_io::onnx;
+use protobuf::rt::vec_packed_enum_data_size;
 
-pub struct Node {
-    name: str,
-    succ: List<i32>,
-    pred: List<i32>,
-    use_s: HashSet<str>,
-    def_s: HashSet<str>,
-    in_s: HashSet<str>,
-    out_s: HashSet<str>
+type Link = Option<Rc<RefCell<IrNode>>>;
+
+#[derive(Debug, Clone)]
+pub struct IrNode {
+    name: String,
+    succ: Vec<Link>,
+    pred: Vec<Link>,
+    use_s: HashSet<String>,
+    def_s: HashSet<String>,
+    in_s: HashSet<String>,
+    out_s: HashSet<String>
 }
 
-pub struct LivenessIr {
-    node: array<Node>
+impl IrNode {
+    pub fn new(name: String) -> IrNode {
+        IrNode {
+            name: name,
+            succ: vec![],
+            pred: vec![],
+            use_s: HashSet::new(),
+            def_s: HashSet::new(),
+            in_s: HashSet::new(),
+            out_s: HashSet::new()
+        }
+    }
 }
 
-pub fn onnx_to_ir(onnx:onnx::ModelProto) -> LivenessIr {
+
+type LivenessIr = Vec<IrNode>;
+
+pub fn onnx_to_ir(onnx :&onnx::ModelProto) -> LivenessIr {
+    let mut ir :LivenessIr = vec![];
+    let g = onnx.get_graph();
+    let mut layer_cnt = 0;
+
+    for onnx_node in g.get_node() {
+        let node_name = format!("{}_{}", onnx_node.get_op_type().to_string(), layer_cnt);
+        let mut irnode = IrNode::new(node_name);
+        ir.push(irnode.clone());
+
+        layer_cnt = layer_cnt + 1;
+    }
 
 
+    ir
 }
 
 pub fn livness_analisys(ir: &mut LivenessIr) {
--- judecca/src/main.rs
+++ judecca/src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::{BufReader};
 use protobuf::{CodedInputStream, Message};
 use judecca::onnx_io::onnx_reader::onnx_read;
+use judecca::liveness::liveness_analysis::onnx_to_ir;
 
 //protoc --rust_out . onnx.proto で生成されたonnx.rsを読み込む
 
@@ -11,5 +12,12 @@ fn main() {
     let mut u = onnx_read("../data/squeezenet/model.onnx");
 
     println!("producer name: {}", u.get_producer_name());
+
+    let ir = onnx_to_ir(&u);
+
+    for i in &ir{
+        println!("judecca node{:?}", i);
+    }
+
 
 }
