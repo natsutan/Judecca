20d01801a624500eeb3a61a1d19c9f4ab4f4e95e
natsutan <YRW03704@nifty.com>
2019-05-30 11:13:02 +0900

    RefCell追加

--- judecca/src/liveness/liveness_analysis.rs
+++ judecca/src/liveness/liveness_analysis.rs
@@ -4,8 +4,9 @@ use std::collections::HashSet;
 
 use crate::onnx_io::onnx;
 use protobuf::rt::vec_packed_enum_data_size;
+use core::borrow::{Borrow, BorrowMut};
 
-type Link = Option<Rc<RefCell<IrNode>>>;
+type Link = RefCell<IrNode>;
 
 #[derive(Debug, Clone)]
 pub struct IrNode {
@@ -19,8 +20,8 @@ pub struct IrNode {
 }
 
 impl IrNode {
-    pub fn new(name: String) -> IrNode {
-        IrNode {
+    pub fn new(name: String) ->RefCell<IrNode> {
+        RefCell::new(IrNode {
             name: name,
             succ: vec![],
             pred: vec![],
@@ -28,12 +29,16 @@ impl IrNode {
             def_s: HashSet::new(),
             in_s: HashSet::new(),
             out_s: HashSet::new()
-        }
+        })
     }
+    pub fn add_output(& mut self, s:&String) {
+        self.use_s.insert(s.clone());
+    }
+
 }
 
 
-type LivenessIr = Vec<IrNode>;
+type LivenessIr = Vec<RefCell<IrNode>>;
 
 pub fn onnx_to_ir(onnx :&onnx::ModelProto) -> LivenessIr {
     let mut ir :LivenessIr = vec![];
@@ -42,11 +47,30 @@ pub fn onnx_to_ir(onnx :&onnx::ModelProto) -> LivenessIr {
 
     for onnx_node in g.get_node() {
         let node_name = format!("{}_{}", onnx_node.get_op_type().to_string(), layer_cnt);
+        // output -> def, input -> use
         let mut irnode = IrNode::new(node_name);
-        ir.push(irnode.clone());
+        for output in onnx_node.get_output() {
+            irnode.borrow_mut().def_s.insert(output.clone());
+        }
+        for input in onnx_node.get_input() {
+            irnode.borrow_mut().use_s.insert(input.clone());
+        }
+
+        ir.push(irnode);
 
         layer_cnt = layer_cnt + 1;
     }
+
+    //make graph
+//    for mut cur_node in ir {
+//        for out in cur_node.borrow_mut().def_s.clone() {
+//            for mut next_node in ir.borrow_mut() {
+//                if next_node.borrow_mut().use_s.contains(&out) {
+//                    cur_node.borrow_mut().succ.push(next_node);
+//                }
+//            }
+//        }
+//    }
 
 
     ir
